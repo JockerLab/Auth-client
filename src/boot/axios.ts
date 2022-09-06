0275389462e211020ffa0014eb0035198115e66b
Всеволод Шалдин <shaldin2000@mail.ru>
2022-09-06 18:06:33 +0300

    Added files

---
+++ src/boot/axios.ts
@@ -0,0 +1,29 @@
+import { boot } from 'quasar/wrappers'
+import axios from 'axios'
+
+const api = axios.create({ baseURL: 'http://localhost:3000' });
+
+api.interceptors.request.use((config) => {
+  if (localStorage.getItem('access_token') && config.headers) {
+    config.headers.Authorization = `bearer ${localStorage.getItem('access_token')}`;
+  }
+  return config;
+}, (error) => {
+  return Promise.reject(error);
+});
+
+api.interceptors.response.use((response) => {
+  if (response.data.access_token) {
+    localStorage.setItem('access_token', response.data.access_token);
+  }
+  return response;
+}, (error) => {
+  return Promise.reject(error);
+});
+
+export default boot(({ app }) => {
+  app.config.globalProperties.$axios = axios
+  app.config.globalProperties.$api = api
+})
+
+export { axios, api }
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-page class="row items-center justify-evenly">
+    <info-component></info-component>
+  </q-page>
+</template>
+
+<script lang="ts">
+  import InfoComponent from 'components/InfoComponent.vue';
+  import { defineComponent } from 'vue';
+
+  export default defineComponent({
+    name: 'IndexPage',
+    components: { InfoComponent },
+  });
+</script>
