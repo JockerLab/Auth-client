9639b86b9448480f1b6963dea8dbf60c2d45f687
Всеволод Шалдин <shaldin2000@mail.ru>
2022-09-07 18:46:38 +0300

    Added logout

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,4 +1,14 @@
 <template>
+    
+    <q-header  class="bg-primary text-white">
+        <q-toolbar class="justify-between">
+            <q-toolbar-title>
+                Auth App
+            </q-toolbar-title>
+            <q-btn @click="logout" label="log-out" color="white" text-color="black" class="q-ml-sm" />
+        </q-toolbar>
+    </q-header>
+
     <q-page class="row items-center justify-evenly">
         <info-component></info-component>
     </q-page>
@@ -7,9 +17,19 @@
 <script lang="ts">
 import InfoComponent from 'components/InfoComponent.vue';
 import { defineComponent } from 'vue';
+import { useRouter } from 'vue-router'
 
 export default defineComponent({
     name: 'IndexPage',
     components: { InfoComponent },
+    setup() {
+        const router = useRouter();
+        return {
+            async logout() {
+                localStorage.removeItem('access_token');
+                router.push({ path: '/login' });
+            }
+        }
+    }
 });
 </script>
